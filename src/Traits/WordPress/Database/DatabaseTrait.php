474db7a5f920ced464f430760c74f2794096891c
Alfonso Martinez <opensaurusrex@gmail.com>
2023-06-26 11:39:13 -0500

    Adding `$this->wpDatabasePrefix = $wpdb->prefix` to `ObsidianMoon\ProjectUtilities\Traits\WordPress\Database\DatabaseTrait`.

--- src/Traits/WordPress/Database/DatabaseTrait.php
+++ src/Traits/WordPress/Database/DatabaseTrait.php
@@ -24,13 +24,19 @@ trait DatabaseTrait
     protected string $sql;
 
     /**
+     * @var string The prefix for WordPress table names
+     */
+    protected string $wpDatabasePrefix;
+
+    /**
      * Get the charset and prefixes.
      */
     public function gatherPrefixes(?string $prefix = null): void
     {
         global $wpdb;
 
-        $this->charsetCollate = $wpdb->get_charset_collate();
-        $this->databasePrefix = $wpdb->prefix . $prefix;
+        $this->charsetCollate   = $wpdb->get_charset_collate();
+        $this->wpDatabasePrefix = $wpdb->prefix;
+        $this->databasePrefix   = $wpdb->prefix . $prefix;
     }
 }
